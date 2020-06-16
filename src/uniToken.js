4c5df978f2764c011af35a3505043669f86c746f
hbcui1984 <hbcui1984@163.com>
2020-06-16 23:12:52 +0800

    Update uniToken.js

--- src/uniToken.js
+++ src/uniToken.js
@@ -1,20 +1,18 @@
 import jwt from 'jsonwebtoken'
-import {
-    tokenSecret
-} from './config.json'
+// import {
+//     tokenSecret
+// } from './config.json'
 
+const tokenSecret = 'your-pwd-sec'
 
-// const tokenSecret = 'your-pwd-sec'
 console.log(tokenSecret);
-
-// let tokenSecret = ""
 
 let uniToken = {
     createToken: function(user) {
         var token = jwt.sign({
             uid: user._id
         }, tokenSecret, {
-            expiresIn: 2
+            expiresIn: 60*60
         });
 
 
@@ -27,11 +25,14 @@ let uniToken = {
 
     checkToken: function(token) {
         try {
+            console.log('checkToken 1');
             var decoded = jwt.verify(token, tokenSecret);
+            console.log('checkToken 2');
             console.log('decoded', decoded);
 
             return decoded
         } catch (err) {
+            console.log('checkToken 3',err);
             return {
                 code: 1301,
                 msg: '非法token',
